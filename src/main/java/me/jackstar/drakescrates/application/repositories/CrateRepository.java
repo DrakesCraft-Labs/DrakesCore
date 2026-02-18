c240bbcced348608338476f80a6ccd72e947f2ab
Pablo Elías Avendaño Miranda <pablo.elias.miranda.292003@gmail.com>
2026-02-18 03:07:14 -0300

    Feat: Implement DrakesCrates Domain & Application Layer

---
+++ src/main/java/me/jackstar/drakescrates/application/repositories/CrateRepository.java
@@ -0,0 +1,22 @@
+package me.jackstar.drakescrates.application.repositories;
+
+import me.jackstar.drakescrates.domain.models.Crate;
+import me.jackstar.drakescrates.domain.models.Key;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface CrateRepository {
+
+    void reload();
+
+    Collection<Crate> getAllCrates();
+
+    Collection<Key> getAllKeys();
+
+    Optional<Crate> findCrateById(String crateId);
+
+    Optional<Key> findKeyById(String keyId);
+
+    Optional<Key> findRequiredKeyForCrate(String crateId);
+}
